194e54379ab30780191c9b60311006e42de08f2f
YASH!!! <kanwar.yashwender2022@vitstudent.ac.in>
2023-09-23 17:08:33 +0530

    added handle func

--- game.py
+++ game.py
@@ -37,7 +37,23 @@ If none of the key words exists in the tokens, return "I don't understand that c
 '''
 def handle_user_command(user_input):
     global current_room, score
-    pass
+    processed_tokens = process_user_input(user_input)
+    if "quit" in processed_tokens:
+        return quit_game()
+    elif "look" in processed_tokens:
+        return look_around()
+    elif "take" in processed_tokens:
+        return take_item(processed_tokens)
+    elif "inventory" in processed_tokens:
+        return inventory_status()
+    elif "go" in processed_tokens:
+        return go_to(processed_tokens)
+    elif "talk" in processed_tokens:
+        return talk_to(processed_tokens)
+    elif "unlock" in processed_tokens:
+        return unlock(processed_tokens)
+    else:
+        return "I don't understand that command."
 
 
 '''
